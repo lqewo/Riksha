4045609b7f977da04122db0d92944502fe95bfd9
lqewo <vrkonosha@gmail.com>
2022-04-07 12:10:47 +0300

    added header & footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
 import * as flsFunction from "./modules/functions.js";
-
+import $ from "jquery";
 flsFunction.isWebp();
 
  import Swiper, { Navigation, Pagination } from 'swiper';
 
   const swiper = new Swiper();
+
+  $(function() {
+    let header = $('.header');
+
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1) {
+       header.addClass('header_fixed');
+      } else {
+       header.removeClass('header_fixed');
+      }
+    });
+   });
