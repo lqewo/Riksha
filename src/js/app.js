0a8b3eae71c7f84d3fd674651def92766df205f2
lqewo <vrkonosha@gmail.com>
2022-04-09 20:09:29 +0300

    added section top-position and combo menu

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,59 @@ flsFunction.isWebp();
 
  import Swiper, { Navigation, Pagination } from 'swiper';
 
-  const swiper = new Swiper();
+new Swiper('.banner__slider', {
+    // Optional parameters
+    modules: [Navigation, Pagination],
 
-  $(function() {
-    let header = $('.header');
+    // If we need pagination
 
-    $(window).scroll(function() {
-      if($(this).scrollTop() > 1) {
-       header.addClass('header_fixed');
-      } else {
-       header.removeClass('header_fixed');
-      }
-    });
-   });
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.banner-swiper-button-next',
+      prevEl: '.banner-swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+
+  });
+
+  new Swiper ('.top-position__slider', {
+    modules: [Navigation, Pagination],
+
+    // If we need pagination
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.top-swiper-button-next',
+      prevEl: '.top-swiper-button-prev',
+    },
+    nested: true,
+  });
+
+  new Swiper ('.combo-menu__slider', {
+    modules: [Navigation, Pagination],
+
+    // If we need pagination
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.combo-menu-swiper-button-next',
+      prevEl: '.combo-menu-swiper-button-prev',
+    },
+    nested: true,
+  });
+
+//   $(function() {
+//     let header = $('.header');
+
+//     $(window).scroll(function() {
+//       if($(this).scrollTop() > 1) {
+//        header.addClass('header_fixed');
+//       } else {
+//        header.removeClass('header_fixed');
+//       }
+//     });
+//    });
