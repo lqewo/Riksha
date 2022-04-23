e86f644269c30ed616e3a4daf5dcc9388f565f11
lqewo <vrkonosha@gmail.com>
2022-04-23 23:14:18 +0300

    added card

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import * as flsFunction from "./modules/functions.js";
 // import $ from "jquery";
 // import styler from "jquery-form-styler/dist/jquery.formstyler.js";
+import "rateyo";
 import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
 flsFunction.isWebp();
 
@@ -51,7 +52,42 @@ new Swiper('.banner__slider', {
     nested: true,
   });
 
+  new Swiper ('.card__img', {
+    modules: [Navigation, Pagination],
+
+    // If we need pagination
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.card__img-button-next',
+      prevEl: '.card__img-button-prev',
+    },
+    nested: true,
+  });
+
   $(function() {
+
+
+    $("#comment__raiting--user-1").rateYo({
+      rating: 4,
+      readOnly: true,
+      starWidth: "16px",
+      spacing: "4px",
+      ratedFill: '#E07153',
+      starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.99978 12.1733L3.29778 14.8053L4.34778 9.51992L0.391113 5.86125L5.74245 5.22659L7.99978 0.333252L10.2571 5.22659L15.6084 5.86125L11.6518 9.51992L12.7018 14.8053L7.99978 12.1733Z"/></svg>'
+    });
+
+    $("#comment__raiting--user-2").rateYo({
+      rating: 5,
+      readOnly: true,
+      starWidth: "16px",
+      spacing: "4px",
+      ratedFill: '#E07153',
+      starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.99978 12.1733L3.29778 14.8053L4.34778 9.51992L0.391113 5.86125L5.74245 5.22659L7.99978 0.333252L10.2571 5.22659L15.6084 5.86125L11.6518 9.51992L12.7018 14.8053L7.99978 12.1733Z"/></svg>'
+    });
+
+
 
 	$('.sort__select').styler();
 
